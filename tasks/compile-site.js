a82fd37ae62bca563145bc12710bdc665ef03db0
Ryan Watkins-Hughes <rwatkins-hughes@continuum.io>
2016-02-15 14:40:24 -0600

    Add Markdown support

--- tasks/compile-site.js
+++ tasks/compile-site.js
@@ -1,15 +1,17 @@
 'use strict';
 const fs = require('fs');
 const jade = require('jade');
+const markdown = require('jstransformer-markdown');
 const config = require(__root + '/config.js');
-const fn = jade.compileFile(__root + config.views + 'pages.jade', config.jade);
+const fn = jade.compileFile(__root + config.views + 'with-markdown.jade', config.jade);
 
 module.exports = function() {
   let locals = JSON.parse(fs.readFileSync(config.data + 'locals.json'));
   // Temp / static pages array
   locals.pages = JSON.parse(fs.readFileSync(config.data + 'pages.json'));
+  
   let html = fn(locals);
-  fs.writeFile(config.build + 'pages.html', html, (error) => {
+  fs.writeFile(config.build + 'with-markdown.html', html, (error) => {
     if (error) throw error;
     console.log('build complete!');
   });
