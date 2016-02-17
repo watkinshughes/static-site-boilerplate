2a8b62e2bde908ac380b3f6519fca49ce2353ec7
Ryan Watkins-Hughes <rwatkins-hughes@continuum.io>
2016-02-17 14:23:48 -0600

    Updates for deploy vs develop

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,17 +1,17 @@
 'use strict'
 const config = require('../config.js')
-const compileHTML = require('../lib/tasks/compile-html.js')
+const compileView = require('../lib/tasks/compile-view.js')
 
 module.exports = {
   compile () {
     let posts = config.locals().posts
     // Just 10 posts for now
     for (let i = 0; i < 10; i++) {
-      let filename = posts[i].title.toLowerCase().replace(/\s+/g, "-") + '.html'
-      compileHTML({
+      let filename = posts[i].title.toLowerCase().replace(/\s+/g, '-') + '.html'
+      compileView({
         'locals': posts[i],
         'destination': config.build + 'posts/' + filename,
-        'view': 'with-markdown'
+        'view': 'content-types/post'
       })
     }
   }
--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict'
 const fetchData = require('./lib/tasks/fetch-data.js')
 const buildSite = require('./lib/tasks/build-site.js')
+const deploy = require('./lib/tasks/deploy.js')
+const develop = require('./lib/tasks/develop.js')
+const env = process.env.ENV
 
-fetchData(buildSite)
+const build = new Promise((resolve, reject) => {
+  // Add error handling
+  fetchData(() => {
+    buildSite()
+    resolve()
+  })
+})
+
+if (env) {
+  build.then(deploy)
+} else {
+  build.then(develop)
+}
