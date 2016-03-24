0d84ff2c15c054f6be28dadee9ec3d3215d8a6a3
Ryan Watkins-Hughes <rwatkins-hughes@continuum.io>
2016-03-24 08:43:14 -0500

    Cleanup

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 const fetchData = require('./lib/tasks/fetch-data.js')
-const buildSite = require('./lib/tasks/build-site.js')
+const buildApp = require('./lib/tasks/build-app.js')
 const deploy = require('./lib/tasks/deploy.js')
 const develop = require('./lib/tasks/develop.js')
 const env = process.env.ENV
 
+
 const build = new Promise((resolve, reject) => {
-  // @todo - Add error handling
   fetchData(() => {
-    buildSite()
+    buildApp()
     resolve()
   })
 })
